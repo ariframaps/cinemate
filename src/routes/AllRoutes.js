375f6d0fd10768552a31b02dd275478de5a4c30e
ariframaps <ariframa@student.ub.ac.id>
2024-04-07 15:19:34 +0700

    membuat header dan footer menggunakan flowbite, serta mengatur navigasi untuk navbarnya

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -6,7 +6,7 @@ export const AllRoutes = () => {
         <Routes>
             <Route path='' element={<MovieList />} />
             <Route path='movie/:id' element={<MovieDetail />} />
-            <Route path='movies/pathp' element={<MovieList />} />
+            <Route path='movies/top' element={<MovieList />} />
             <Route path='movies/popular' element={<MovieList />} />
             <Route path='movies/upcoming' element={<MovieList />} />
             <Route path='search' element={<Search />} />
