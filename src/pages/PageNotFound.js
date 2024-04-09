c299f5f02024dd24ff5f67bb679f8a5d275634fc
ariframaps <ariframa@student.ub.ac.id>
2024-04-09 10:27:30 +0700

    membuat page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const PageNotFound = () => {
     return (
-        <main>PageNotFound</main>
+        <main className='flex w-screen-max justify-center items-center'>
+            <div className='text-center'>
+                <h1 className='text-6xl font-bold mb-10 dark:text-white'>404 Page Not Found</h1>
+                <p className='text-xl font-medium dark:text-white'>Whoops! That page doesn’t exist. But do not fret, check out our other resources from below to get started.</p>
+            </div>
+        </main>
     )
 }
